57f5d26e543aae6c6275dc3de19229223cbd37b0
1glam1 <102163352+1glam1@users.noreply.github.com>
2023-10-27 01:57:01 +0500

    Изменения программы
    1. Была добавлена проверка ввода 
    2. Добавлена поддержка русского языка(Ru)
    3. Заглавные буквы остаются заглавными, строчные буквы остаюься строчными

--- Фт-220007 Корпусев.py
+++ Фт-220007 Корпусев.py
@@ -1,28 +1,55 @@
 class Caezar():
-    def __init__(self,text,shift):
+    def __init__(self,text,shift,lang):
         self.text = text 
         self.shift = shift 
+        self.lang = lang
     def encrypted_cipher(self,text,shift):
         names = []
-        names.append(text.upper())
+        names.append(text)
         names.append(' Сдвиг: '+str(shift))
         return ';'.join(names)
-    def decrypted_cipher(self,text,shift):
+    def decrypted_cipher(self,text,shift,lang):
         result = ''
-        for i in text:
-            if i.isalpha():
-                is_upper = i.isupper()
-                shifted_i = chr(((ord(i) - ord('a') + shift) % 26) + ord('a'))
-                if is_upper:
-                    shifted_i = shifted_i.upper()
-                result += shifted_i
-            else:
-                result += i
-        return result.upper()
+        if lang == 'Ru':
+            for i in text:
+                if i.isalpha():
+                    is_upper = i.isupper()
+                    i = i.lower()
+                    shifted_i = chr(((ord(i) - ord('а') + shift) % 33) + ord('а'))
+                    if is_upper:
+                        shifted_i = shifted_i.upper()
+                    result += shifted_i
+                else:
+                    result += i
+        if lang == 'En':
+            for i in text:
+                if i.isalpha():
+                    is_upper = i.isupper()
+                    i = i.lower()
+                    shifted_i = chr(((ord(i) - ord('a') + shift) % 26) + ord('a'))
+                    if is_upper:
+                        shifted_i = shifted_i.upper()
+                    result += shifted_i
+                else:
+                    result += i
+        return result
     
-main = Caezar(input('Введите текст для шифрования: '),int(input("Введите сдвиг (целое число): ")))
-
-encrypted = main.encrypted_cipher(main.text,main.shift)
-decrypted = main.decrypted_cipher(main.text,main.shift)
-print("Зашифрованный текст:", encrypted)
-print("Расшифрованный текст:", decrypted)
+text = input('Введите текст для шифрования: ')
+while True:
+    try:
+        shift = int(input('Введите сдвиг (целое число): '))
+        while True:
+            lang = input('Выберите язык Ru/En: ')
+            if lang == 'Ru' or lang == 'En':
+                main = Caezar(text,shift,lang)
+                encrypted = main.encrypted_cipher(text,shift)
+                decrypted = main.decrypted_cipher(text,shift,lang)
+                print('Зашифрованный текст:', encrypted)
+                print('Расшифрованный текст:', decrypted)
+                break
+            else:
+                print('Введено неверное значение!')
+                continue
+        break
+    except ValueError:
+        print('Введено неверное значение!')
